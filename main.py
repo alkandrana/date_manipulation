138da4fdc7704479aacf9abbd66c6d3f62ab3d96
alkandrana <alkandrana@gmail.com>
2026-05-26 15:11:37 -0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,5 @@
+# CSV (ish) file: each line represents a duration (two dates)
+# Load the data by converting each line to a timedelta (duration) value
+# Add the list of durations together
+# Convert as needed (hours, minutes)
+# Return final duration
