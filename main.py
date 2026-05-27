48469d8c52d4297c1cc32a3da96f67975a50122d
alkandrana <alkandrana@gmail.com>
2026-05-26 19:30:53 -0700

    Added a function to load dates from a csv file.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import csv
+
+
 # CSV (ish) file: each line represents a duration (two dates)
+def load_data(filename):
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+    return rows
+
 # Load the data by converting each line to a timedelta (duration) value
+def get_duration(time_list):
+    
 # Add the list of durations together
 # Convert as needed (hours, minutes)
 # Return final duration
+
+
+print(load_data("dates.csv"))
